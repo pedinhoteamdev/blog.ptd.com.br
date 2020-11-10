cd293d0fc7f61b27c8828078df0bb3c4e563a7fa
metodologiaead <eadmetodologia@gmail.com>
2020-11-10 18:17:49 -0300

    estudo modulo 6 e 7

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,78 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// Para adicionar o campo slug a cada postagem
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
+    // Garante que estamos processando apenas arquivos de redução
+    if (node.internal.type === "MarkdownRemark") {
+      // Use `createFilePath` para transformar arquivos markdown em nosso diretório` data / faqs` em `/ faqs / slug`
+      const slug = createFilePath({
+        node,
+        getNode,
+        basePath: "pages",
+      })
+  
+      // Cria um novo campo consultável com o nome de 'slug'
+      createNodeField({
+        node,
+        name: "slug",
+        value: `/${slug.slice(12)}`,
+      })
+    }
+  }
+
+  exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    return graphql(`
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+    `).then(result => {
+      const posts = result.data.allMarkdownRemark.edges
+
+      posts.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve("./src/templates/blog-post.js"),
+          context: {
+            slug:node.fields.slug
+          }
+        })
+      })
+
+      const postsPerPage = 6
+      const numPages = Math.ceil(posts.length / postsPerPage)
+  
+      Array.from({ length: numPages }).forEach((_, index) => {
+        createPage({
+          path: index === 0 ? `/` : `/page/${index + 1}`,
+          component: path.resolve(`./src/templates/blog-list.js`),
+          context: {
+            limit: postsPerPage,
+            skip: index * postsPerPage,
+            numPages,
+            currentPage: index + 1,
+          },
+        })
+      })
+    })
+  }
---
+++ src/templates/blog-post.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+
+const BlogPost = ({ data }) => {
+  
+    const post = data.markdownRemark
+
+    return (
+        <>
+          <h1>{post.frontmatter.title}</h1>
+       
+        <div dangerouslySetInnerHTML={{ __html: post.html}}></div>
+        </>
+        
+    )
+}
+
+export const query = graphql`
+query Cursos($slug: String) {
+    markdownRemark(fields: {slug: {eq: $slug}}) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`
+    
+
+export default BlogPost
